111515d7c2028689b1607579ef313e226f3b2c51
Momna Saeed <kiranhaseeb094@gmail.com>
2026-07-17 14:22:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Variable Declarations
+
+let developerName = "Momna Saeed";
+
+const bootcampWeek = "Week 7";
+
+let currentTrack = prompt("Enter your current software engineering track:");
+
+// Alert Output
+
+alert(
+"Hello " +
+developerName +
+"! Welcome to " +
+bootcampWeek +
+". Your track is: " +
+currentTrack
+);
+
+// Console Output
+
+console.log("========== Developer Report ==========");
+console.log("Developer Name:", developerName);
+console.log("Bootcamp Week:", bootcampWeek);
+console.log("Current Track:", currentTrack);
+console.log("Status: JavaScript Connected Successfully.");
+console.log("======================================");
